079b44d7c4190e224a2819f36a05c438b5a22691
Rodrigo Queiroz <rodrigo.queiroz.chagas@gmail.com>
2018-07-16 15:45:27 -0300

    Adjustes

--- js/script.js
+++ js/script.js
@@ -1,9 +1,21 @@
 $(function(){
+    //navegação
     $(".nav-menu-hover").hover(function(){
        $(this).prev().toggle();
     })
     
     $(".nav-menu-hover").prev().hover(function(){
        $(this).toggle();
+    })
+    
+    //ver mais
+    $(".ver-mais-btn").on("click", function(){
+        $(this).next().toggle();
+        if($(this).next().is(":visible")){
+            $(this).text("Ver menos");
+        }
+        if($(this).next().is(":hidden")){
+            $(this).text("Ver mais");
+        }
     })
 });
