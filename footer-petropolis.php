3f247d3130b5129352875071e8ace7ec3401d1b8
Rodrigo Queiroz <rodrigo.queiroz.chagas@gmail.com>
2018-07-29 19:17:04 -0300

    Wordpress config

---
+++ footer-petropolis.php
@@ -0,0 +1,13 @@
+    <footer>
+        <p>&copy; 2018 Salva Vidas - Todos os direitos reservados</p>
+    </footer>
+    <script
+        src="https://code.jquery.com/jquery-3.3.1.min.js"
+        integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8="
+        crossorigin="anonymous">
+    </script>
+		
+    <script src="<?php echo get_template_directory_uri() . '/js/script.js'?>"></script>
+</body>
+
+</html>
