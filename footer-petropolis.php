4b6cf45ca8d3537467de2d4c53bf6395090ec394
Rodrigo Queiroz <rodrigo.queiroz.chagas@gmail.com>
2018-07-29 19:40:49 -0300

    jQuery fixed

--- footer-petropolis.php
+++ footer-petropolis.php
@@ -2,9 +2,7 @@
         <p>&copy; 2018 Salva Vidas - Todos os direitos reservados</p>
     </footer>
     <script
-        src="https://code.jquery.com/jquery-3.3.1.min.js"
-        integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8="
-        crossorigin="anonymous">
+        src="https://code.jquery.com/jquery-3.3.1.min.js">
     </script>
 		
     <script src="<?php echo get_template_directory_uri() . '/js/script.js'?>"></script>
